d137397167747e77cf68ee893437d34f4a712f63
abdoulaye <abdelraul140385@gmail.com>
2022-12-05 23:15:33 +0000

    ajout du bouton competence

--- js/script.js
+++ js/script.js
@@ -3,32 +3,33 @@ fetch('js/data.json')
         return res.json()
     })
     .then(function(data){
-        // Photo de profile
+    // Photo de profile
         document.querySelector('.hero .hero_header #hero_image').setAttribute('src', data.profile.photo_profil)
-        // nom
+    // nom
         document.getElementById('hero_title').innerHTML = data.profile.nom
-        //role
+    //role
         document.querySelector('.hero_header .profil').innerHTML = data.profile.profil
 
-        // ma description
+    // ma description
         document.querySelector('.hero #paragraphe').innerHTML = data.profile.description
 
-        // le lien de l'adresse email
+    // le lien de l'adresse email
         document.querySelector('.hero_content .hero_email').textContent = data.profile.email
         document.querySelector('.hero_content .hero_email ').setAttribute('href', 'mailto:'+data.profile.email)
 
         
 
-        //lien telephone
+    //lien telephone
         document.querySelector('.content .numero ').textContent = data.profile.numero
         document.querySelector('.content .numero ').setAttribute('href', 'Tel:'+data.profile.numero)
 
-        // github
+    // lien github
+        document.querySelector('.content .hero_github ').textContent = data.profile.content_lien
         document.querySelector('.content .hero_github ').setAttribute('href', data.profile.github)
 
-        // creation de la liste des compétences
+    // creation de la liste des compétences
   
-        //list item à construire avec javascript
+        // list item à construire avec javascript
         // <div class="skill-item">
         //     <span>30%</span>
         //     <div class="skill">
@@ -38,107 +39,135 @@ fetch('js/data.json')
         //     </div>
         // </div>
 
-//         let skills = data.competences
-//         let skillsListView = document.querySelector('.skills .skills-list')
 
-//         // pour chaque compétence dans sKIlls
-//         for (const skill of skills) {
-            
-//             let skillItem = document.createElement('div')
-//             skillItem.classList.add('skill-item')
+        const formSectionCompetence = document.getElementById('form-section-competence')
+        const competenceForm = document.getElementById('competenceForm')
+        const buttonShowForm = document.getElementById('addButtonCompetence')
 
-//             let skillSpan = document.createElement('span')
-//             skillSpan.textContent = skill.progress
-//             // ajout du span au skill item
-//             skillItem.appendChild(skillSpan);
+        let competences = []
 
-//             // bloc info
-//             let skillInfo = document.createElement('div')
-//             skillInfo.classList.add('skill')
+        // function showForm(e) {
+        //     formSection.classList.remove('d-none')
+        //     e.classList.add('disabled')
+        // }
 
-//             // titre
-//             let skillTitle = document.createElement('h3')
-//             skillTitle.classList.add('title')
-//             skillTitle.textContent = skill.name
+        buttonShowForm.addEventListener('click', function handleClick(e) {
 
-//             // description
-//             let skillDesc = document.createElement('p')
-//             skillDesc.classList.add('description')
-//             skillDesc.textContent = skill.description
+            formSectionCompetence.classList.remove('d-none')
+                e.classList.add('disabled')
+        });
 
-//             // ajout de title et description au bloc info
-//             skillInfo.appendChild(skillTitle)
-//             skillInfo.appendChild(skillDesc)
+        function hideForm() {
+            formSectionCompetence.classList.add('d-none')
+            buttonShowForm.classList.remove('disabled')
+            competenceForm.reset()
+        }
 
-//             // ajout du skill Info au skill item
-//             skillItem.appendChild(skillInfo)
+        competenceForm.addEventListener('submit', function(e) {
+            e.preventDefault()
 
-//             // ajouter l'item dans la list
-//             skillsListView.appendChild(skillItem)
-//         }
+            const data = new FormData(competenceForm)
+            addCompetence(data)
+            hideForm()
+        })
+
+        function addCompetence(formData) {
+            const competence = {
+                'pourcent' : formData.get('pourcent'),
+                'title' : formData.get('title'),
+                'description' : formData.get('description')
+            }
+
+            competences.push(competence)
+            loadCompetences()
+        }
+
+
+        function loadCompetences() {
+            let listView = document.getElementById('liste-competence')
+            listView.innerHTML = ""
+
+            for (const competenc of competences) {
+                let template = `
+                    <div class="col-sm-6 col-md-4 col-xl-3 mb-3">
+                    <div class="competence-item d-flex align-items-center border border-secondary rounded p-2">
+                        <span class="competence-progress fs-1">${competenc.pourcent}%</span>
+                        <div class="competence-details ms-3">
+                            <h3>${competenc.title}</h3>
+                            <p>${competenc.description}</p>
+                        </div>
+                    </div>
+                </div>
+                `
+
+                listView.innerHTML += template
+            }
+        }
+
         
-//         // creation de la liste des langues
-//         // <div class="lang-item">
-//         //     <img src="images/us.png" alt="drapeau americain" class="lang-flag">
-//         //     <span>Anglais</span>
-//         // </div>
 
-//         let langues = data.langues
-//         let languesListView = document.querySelector('.lang .langs-list')
+        // creation de la liste des langues 
 
-//         // pour chauque lang dans langues
-//         for (const lang of langues) {
-//             // bloc item
-//             let langItem = document.createElement('div')
-//             langItem.classList.add('lang-item')
+        const formSectionLangue = document.getElementById('form-section-langue')
+        const langueForm = document.getElementById('langueForm')
+        const buttonShowForml = document.getElementById('addButtonLangue')
 
-//             // drapeau
-//             let langFlag = document.createElement('img')
-//             langFlag.classList.add('lang-flag')
-//             langFlag.setAttribute('src', lang.flag)
-//             langFlag.setAttribute('alt', lang.alt)
-            
-//             // titre
-//             let langName = document.createElement('span')
-//             langName.textContent = lang.name
+        let langues = []
 
-//             //ajout du drapeau et le nom à l'item
-//             langItem.appendChild(langFlag)
-//             langItem.appendChild(langName)
+        // function showForm(e) {
+        //     formSection.classList.remove('d-none')
+        //     e.classList.add('disabled')
+        // }
 
-//             // ajout de l'item à la list
-//             languesListView.appendChild(langItem)
-//         }
+        buttonShowForml.addEventListener('click', function handleClick(e) {
 
-//         //creation de la liste d'expérience
-//         // avec une nouvelle methode
+            formSectionLangue.classList.remove('d-none')
+                e.classList.add('disabled')
+        });
 
-//         // <div class="expert-item">
-//         //     <div class="tagline">
-//         //         <span class="dot"></span>
-//         //         <hr>
-//         //     </div>
-//         //     <p class="tag-data">2018 : Étudiant</p>
-//         // </div>
+        function hideForm() {
+            formSectionLangue.classList.add('d-none')
+            buttonShowForml.classList.remove('disabled')
+            langueForm.reset()
+        }
 
-//         let experiences = data.experiences
-//         let experView = document.querySelector('.expert .expert-list')
+        langueForm.addEventListener('submit', function(e) {
+            e.preventDefault()
 
-//         for (const experience of experiences) {
-//             // creation du bloc 
-//             let expItem = document.createElement('div')
-//             expItem.classList.add('expert-item')
+            const data = new FormData(langueForm)
+            addLangue(data)
+            hideForm()
+        })
 
-//             expItem.innerHTML = `
-//                 <div class="tagline">
-//                    <span class="dot"></span>
-//                    <hr>
-//                 </div>
-//                 <p class="tag-data">${experience.year} : ${experience.role}</p>
-//             `
+        function addLangue(formData) {
+            const langue = {
+                'image' : formData.get('image'),
+                'langue' : formData.get('langue'),
+                
+            }
 
-//             // ajout de l'item dans la liste
-//             experView.appendChild(expItem)
-//         }
+            langues.push(langue)
+            loadLangues()
+        }
+
+
+        function loadLangues() {
+            let listView = document.getElementById('listes-langues')
+            listView.innerHTML = ""
+
+            for (const lang of langues) {
+                let template = `
+                    <div class="col-sm-6 col-md-4 col-xl-3 mb-3">
+                        <img  src"./images/${lang.image}" alt="photo" class="image">
+                        <span class="fs-1">${lang.langue}</span>
+                    </div>
+                </div>
+                `
+
+                listView.innerHTML += template
+            }
+        }
+
+        
     });
         
